2eddf35d1be47dc3bfbb7e00ec3ce19c27b93192
Derek Poston <Hamersgaven01@gmail.com>
2022-01-12 09:09:38 -0600

    queries

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,6 +1,18 @@
---Part 1
+-- Part 1 
+SET FOREIGN_KEY_CHECKS=0;
+-- Id INT, 
+-- Name VARCHAR(255),
+-- EmployerId INT
 
---Part 2
+-- --Part 2
 
---Part 3
+SELECT Name
+FROM Employers
+WHERE location = "St. Louis City" ;
 
+-- --Part 3
+SELECT Name, Description
+FROM Skills
+LEFT JOIN JobSkills 
+ON skills.ID = JobSkills.SkillId 
+WHERE JobSkills.JobId IS NOT NULL ORDER BY Name ASC;
